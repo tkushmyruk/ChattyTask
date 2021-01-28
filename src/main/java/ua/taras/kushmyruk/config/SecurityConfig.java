b03e17a6c3b440914ff10945395b36f1272fd8f3
tkushmyruk <taras.kushmyruk@gmail.com>
2021-01-28 13:37:01 +0200

    Without-.gitignore-Chatty-Web-Application

--- src/main/java/ua/taras/kushmyruk/config/SecurityConfig.java
+++ src/main/java/ua/taras/kushmyruk/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package ua.taras.kushmyruk.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 import ua.taras.kushmyruk.service.serviceImpl.UserServiceImpl;
 
